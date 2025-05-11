b283166f66e03690f65044e801bdc73134779286
Tia Hedlund <tia.hedlund@elev.ga.ntig.se>
2025-05-11 18:12:53 +0200

    Skapade första metod för att uppgradera zoot, men kommer troligen ändra antalet wood

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Zoo {
     public String name;
     private static double money = 0;
@@ -23,16 +25,42 @@ public class Zoo {
         return money;
     }
 
-    public void upgrade(){
-        Scanner myScanner =
-        System.out.println("Choose a method to upgrade the Zoo");
+    public void upgradeMenu(Inventory inventory){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Choose a method to upgrade the Zoo [1/2/3]");
         System.out.println("1. Use wood");
         System.out.println("2. Pay money");
-        System.out.println("Back.");
+        System.out.println("3. Back.");
 
-        int choice =
-        switch ()
-        zooLevel++;
+        String choice = myScanner.nextLine();
+        switch (choice){
+            case "1":
+                UpgradeZooWood(inventory);
+                break;
+            case "2":
+                // UpgradeZooMoney();
+                break;
+            case "3":
+                break;
+            default:
+                System.out.println("Invalid choice, please enter: [1/2/3]");
+        }
+
     }
+
+    public void UpgradeZooWood(Inventory inventory){
+        double woodNeeded = this.zooLevel+this.zooLevel*0.5;
+        int intWoodNeeded = (int) Math.round(woodNeeded);
+
+        if (inventory.woodCount >= intWoodNeeded) {
+            this.zooLevel++;
+            System.out.println("Zoo have been updraded to level "+zooLevel);
+        }
+        else{
+            System.out.println(inventory.woodCount+"/"+intWoodNeeded+ ". Not enough wood. "+(intWoodNeeded-inventory.woodCount)+ " more wood required.");
+        }
+
+    }
+
 
 }
